41ce540106f4eb10fa56fd5d8846806439479c8c
Igor Afonso <igorafonsonunes@outlook.com>
2019-09-18 16:44:59 -0300

    Criando projeto

---
+++ TabelaFipe/TabelaFipeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TabelaFipeTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TabelaFipeTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5f734144-20c3-421c-afff-76bb2432b27f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TabelaFipe/TabelaFipeTest/TabelaFipeTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TabelaFipe.Models;
+
+
+namespace TabelaFipeTest
+{
+    [TestClass]
+    public class TabelaFipeTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var tabela = new TabelaFipe.Models.TabelaFipe();
+
+            var resultado = tabela.GetTabelaFipe("carros", "veiculos","21");
+
+            Assert.IsNotNull(resultado);
+
+        }
+    }
+}
