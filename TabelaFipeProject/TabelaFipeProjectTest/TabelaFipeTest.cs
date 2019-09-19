9b99970ca5b16f95d5a3b487ed0ba11559fc3601
Igor Afonso <igorafonsonunes@outlook.com>
2019-09-19 00:17:19 -0300

    Criação do projeto novo

---
+++ TabelaFipeProject/TabelaFipeProjectTest/TabelaFipeTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TabelaFipeProject;
+
+namespace TabelaFipeProjectTest
+{
+    [TestClass]
+    public class TabelaFipeTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var tabela = new TabelaFipe();
+
+            var resultado = tabela.GetTabelaFipe("carros", "veiculos", "21");
+
+            Assert.IsNotNull(resultado);
+
+        }
+    }
+}
